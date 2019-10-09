62976c00001ce0c6ba2170f3625de1e694811ad9
DouDe <zhizhiqiu@qq.com>
2019-10-09 09:10:37 +0800

    增加100Easy题解 :confused:

--- test/Test.java
+++ test/Test.java
@@ -1,33 +1,57 @@
-import java.util.HashMap;
 
 public  class Test{
 
     public static void main(String args[]) {
-        String s = "new Hello World! ";
-        System.out.println(sulotion(s));
-        String ss = "";
-        System.out.println(ss.length());
+        ListNode node = new ListNode(345);
+        ListNode node2 = new ListNode(345);
+
+        ListNode node1 = new Test().addTwoNumbers(node, node2);
+        System.out.println(node1.val);
 
     }
     public static String sulotion(String nums ){
+        return null;
 
-        if (nums == null ||nums.length() ==0 )
-            return " ";
+    }
+    public static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x ){ val = x;}
+    }
 
-        //按照空格切分s
-        String[] array = nums.split(" ");
-        StringBuilder builder = new StringBuilder();
-        //在builder中插入单词
-        for (int i = 0; i<array.length;i++){
-            if (!array[i].equals("")){
-                if (builder.length() > 0){
-                    builder.insert(0," ");
-                }
-                builder.insert(0,array[i]);
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        int n1,n2;
+        int s,ca;
+        ca =0;
+        s =0;
+        ListNode node =null;
+        ListNode pre = new ListNode(-1);
+        ListNode head = pre;
+        while (l1 != null || l2 != null){
+            n1=n2=0;
+            if (l1 != null){
+                n1 = l1.val;
+                l1=  l1.next;
             }
-        }
+            if (l2 != null){
+                n2 = l2.val;
+                l2 = l2.next;
+            }
+            s= n1+n2+ca;
+            //进位和  和
+            if ((ca = s/10) == 1 && (s=s-10) > 0);
 
-        return builder.toString();
+            node = new ListNode(s);
+            pre.next = node;
+            pre = node;
+        }
+        // 处理最后有进位的情况
+        if (ca ==1){
+            node = new ListNode(1);
+            node.next = null;
+            pre.next = node;
+        }
+        return  head.next;
     }
 
 }
